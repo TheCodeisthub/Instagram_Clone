c9735628023315e0d603c7170822678caecef1dc
Roosevelt <howdy.roosevelt@gmail.com>
2019-12-05 21:41:51 +0100

    Add: Login Feature

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/screens/signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  static final String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print(_email);
+      print(_password);
+      // Logging in the user w/ firebase
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram',
+              style: TextStyle(fontFamily: 'Billabong', fontSize: 50.0),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                      validator: (input) => !input.contains('@')
+                          ? 'Please enter a valid email'
+                          : null,
+                      onSaved: (input) => _email = input,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      validator: (input) => input.length < 6
+                          ? 'Must be at least 6 characters'
+                          : null,
+                      onSaved: (input) => _email = input,
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 50.0),
+                  Container(
+                    width: 150.0,
+                    child: FlatButton(
+                      onPressed: _submit,
+                      color: Colors.pink,
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed: () =>
+                          Navigator.pushNamed(context, SignupScreen.id),
+                      padding: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: Text(
+                          'Create an account',
+                          style: TextStyle(
+                            color: Colors.pink,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  static final String id = 'signup_screen';
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print(_email);
+      print(_password);
+      // Logging in the user w/ firebase
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram',
+              style: TextStyle(fontFamily: 'Billabong', fontSize: 50.0),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                      validator: (input) => !input.contains('@')
+                          ? 'Please enter a valid email'
+                          : null,
+                      onSaved: (input) => _email = input,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      validator: (input) => input.length < 6
+                          ? 'Must be at least 6 characters'
+                          : null,
+                      onSaved: (input) => _email = input,
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 50.0),
+                  Container(
+                    width: 150.0,
+                    child: FlatButton(
+                      onPressed: _submit,
+                      color: Colors.pink,
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed: () => Navigator.pop(context),
+                      padding: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: Text(
+                          'Have an account? Login',
+                          style: TextStyle(
+                            color: Colors.pink,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
